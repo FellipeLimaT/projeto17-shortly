c4e52b33affd1f2c53e3e565149c1319f945e505
fellipelima <fellipelimatorres@gmail.com>
2023-03-16 14:14:30 -0300

    feat: sign in and sign up

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/schemaValidator.js";
 import userSchema from "../schemas/userSchema.js";
 import loginSchema from "../schemas/loginSchema.js";
+import { createUser } from "../controllers/users.controller.js"
+import { signIn } from "../controllers/auth.controller.js";
 
 const router = Router();
 
-router.post("/signin", validateSchema(loginSchema), () => { });
-router.post("/signup", validateSchema(userSchema), () => { });
+router.post("/signin", validateSchema(loginSchema), signIn);
+router.post("/signup", validateSchema(userSchema), createUser);
 
 export default router;
